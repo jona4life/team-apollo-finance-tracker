30fa32b993dca950827dd5dcdc96ca1e48768797
Feyisola Jonathan <jonathanfeyisola@gmail.com>
2019-10-05 06:42:36 +0100

    Add files via upload

---
+++ web/js/checkLoggedIn.js
@@ -0,0 +1,28 @@
+async function sendRequest(url,data){
+
+    function formEncode(obj) {
+    var str = [];
+    for(var p in obj)
+    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    return str.join("&");
+    }
+    
+    var dat = await fetch(url, {
+    method: 'POST',
+    headers: { "Content-type": "application/x-www-form-urlencoded"},
+     credentials: "include",
+    body: formEncode(data)
+    }).then(res => res.json())
+    .then(response => JSON.stringify(response))
+    .catch(error => console.error('Error: '+error));
+    
+    return JSON.parse(dat);
+    
+}
+
+let check = sendRequest("./backend/checkSession.php", {});
+check.then(function(data){
+    if (data['error'] != 0){
+        window.location.assign("/apollo%20finance/index.html")
+    }
+})
